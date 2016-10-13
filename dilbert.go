21f1c5d9a1d5732261c71989365409d2264d98d5
ampersand <ampersand@mba.local>
2016-10-13 22:28:46 +0100

    Image file now dowloaded and saved Start positioning date format

--- dilbert.go
+++ dilbert.go
@@ -7,10 +7,23 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"time"
 )
 
-func getDilbertImage(path string) (myerr error) {
-	fmt.Println(path)
+func getDilbertImage(date string, path string) (filename string, myerr error) {
+	if res, err := http.Get(path); err == nil {
+		if image, err := ioutil.ReadAll(res.Body); err == nil {
+			defer res.Body.Close()
+			filename = "./" + date + ".gif"
+			if err := ioutil.WriteFile(filename, image, 0644); err != nil {
+				myerr = err
+			}
+		} else {
+			myerr = err
+		}
+	} else {
+		myerr = err
+	}
 	return
 }
 
@@ -18,12 +31,10 @@ func getDilbertImagePath(date string) (path string, myerr error) {
 	var imgRegEx = regexp.MustCompile(`data-image="http:\/\/assets\.amuniversal.com\/([a-z0-9]+)"\s`)
 	if res, err := http.Get("http://dilbert.com/strip/" + date); err == nil {
 		if res.StatusCode == 200 {
-			// If page loaded, open document body
 			body, err := ioutil.ReadAll(res.Body)
 			if err == nil {
 				defer res.Body.Close()
 
-				// Find the image path for the strip using RegEx
 				if imgRegExMatches := imgRegEx.FindAllSubmatch(body, 1); len(imgRegExMatches) == 1 {
 					path = "http://assets.amuniversal.com/" + string(imgRegExMatches[0][1])
 				} else {
@@ -42,37 +53,16 @@ func getDilbertImagePath(date string) (path string, myerr error) {
 }
 
 func main() {
-	date := "2016-10-12"
+	//date := "2016-10-12"
+	date := time.Now().Format("2006-01-02")
 	if path, err := getDilbertImagePath(date); err == nil {
-		getDilbertImage(path)
+		fmt.Println(path, "found")
+		if filename, err := getDilbertImage(date, path); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println(filename, "saved")
+		}
 	} else {
 		log.Fatal(err)
 	}
-	// if err := getDilbertImage(path); err == nil {
-	// 	fmt.Println("Dilbert image for " + date + " downloaded")
-	// }
-
-	/*
-				if res2, err := http.Get(imgURL); err == nil && res2.StatusCode == 200 && res2.Header.Get("Content-Type") != "text/html; charset=utf-8" {
-					image, err := ioutil.ReadAll(res2.Body)
-					if err == nil {
-						defer res2.Body.Close()
-						fmt.Println("Loaded image")
-						// save it to disk
-						err := ioutil.WriteFile("./today.gif", image, 0644)
-						if err != nil {
-							log.Fatal(err)
-						}
-					} else {
-						log.Fatal("Failed to load image")
-					}
-				} else {
-					log.Fatal("Failed to get image from RegEx")
-				}
-			}
-		} else {
-			// Epic fail, we've not got page from dilbert.com
-			log.Fatalf("%s\n", res1.Status)
-		}
-	*/
 }
